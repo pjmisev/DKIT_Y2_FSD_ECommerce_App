565ed96bccc5ea565aef85beec1e8c3076416b79
RaivisK2510 <raiviskrums11@gmail.com>
2026-02-19 16:06:07 +0000

    - Load JWT private key from file to enhance security configuration - Add project-specific Material Theme settings

--- server/routes/products.js
+++ server/routes/products.js
@@ -4,6 +4,9 @@ const {validateString, validatePrice, validateDate} = require("../lib/validation
 const createError = require("http-errors");
 const jwt = require('jsonwebtoken')
 
+const fs = require('fs')
+const JWT_PRIVATE_KEY = fs.readFileSync(process.env.JWT_PRIVATE_KEY_FILENAME, 'utf8')
+
 router.get(`/api/products`, (req, res, next) =>
 {
     productsModel.find({})
@@ -29,7 +32,7 @@ router.get(`/api/products/:id`, (req, res, next) => {
 
 router.post(`/api/products`, (req, res, next) =>
 {
-    jwt.verify(req.headers.authorization, process.env.JWT_PRIVATE_KEY, {algorithm: "HS256"}, (err, decodedToken) =>
+    jwt.verify(req.headers.authorization, JWT_PRIVATE_KEY, {algorithm: "HS256"}, (err, decodedToken) =>
     {
         if (err)
         {
