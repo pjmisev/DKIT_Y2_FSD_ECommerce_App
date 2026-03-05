00c4cb172efba32938fd554250146fdb84a38a35
pjmisev <pijus.misevicius@outlook.com>
2026-03-05 14:50:56 +0000

    Downgrade `react-router` and `react-router-dom` dependencies for compatibility and fix import usage in `DisplayProduct` component.

--- client/src/components/DisplayProduct.jsx
+++ client/src/components/DisplayProduct.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
-import {Link} from "react-router";
+import { Link, useParams } from "react-router-dom";
 
 export const DisplayProduct = () => {
     const { id } = useParams();
